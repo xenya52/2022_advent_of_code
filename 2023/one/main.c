e2fbf231fe2630e80b31143cace9144a532026fe
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-12-02 23:36:04 +0100

    backup

--- 2023/one/main.c
+++ 2023/one/main.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ASCIIDOWGRADE 48
+#define ASCII_DOWGRADE 48
+#define MAX_LINE_LENGTH 100
 
 int main(int argc, char *argv[]) {
+    if(!argv[1]) {
+        printf("You need to give the first parameter as the file name");
+        return -1;
+    }
+
     FILE *f_ptr = fopen(argv[1], "r");
+    if(f_ptr == NULL) {
+        printf("You given files is invalid");
+        return -1;
+    }
     
+    char *cur_line;
     char index;
     char temp_first = 0;
     char temp_last = 0;
     int sum = 0;
     int is_first_in = 0; //bool
 
+    while (index != EOF) {
+        index = fgetc(f_ptr);
+        if(index == '\n') {
+            printf("%s\n",cur_line);
+        }
+    }
+/*
     if(f_ptr != NULL) {
         while (index != EOF) {
             index = fgetc(f_ptr);
@@ -33,7 +51,7 @@ int main(int argc, char *argv[]) {
     else {
         printf("Error: No file / Not able to open");
     }
-
+*/
     printf("%d",sum);
 
     fclose(f_ptr);
