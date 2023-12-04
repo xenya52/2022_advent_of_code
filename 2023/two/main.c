53ee82e399b1f6e20289d5419e08b872a50b42e7
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-12-04 22:13:50 +0100

    backup commit

--- 2023/two/main.c
+++ 2023/two/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stbool.h>
+
+#define B_BORDER    14
+#define G_BORDER    13
+#define R_BORDER    12
 
 int main(int argc, char *argv[]) {
     if(!argv[1]) {
@@ -11,5 +16,27 @@ int main(int argc, char *argv[]) {
         printf("You given files is invalid");
         return -1;
     }
-    printf("Hello mum");
+    char index = '';
+    char index_negativ
+    int game = 0;
+    int b = 0;
+    int g = 0;
+    int r = 0;
+    int sum = 0;
+    if(f_ptr != NULL) {
+        while (index != EOF) {
+            //After every game..
+            if (char == '\n') {
+                if (b < B_BORDER && r < R_BORDER && g < G_BORDER) sum += game;
+                game = 0;
+                b = 0;
+                r = 0;
+                g = 0;
+            }
+            else {
+                if (index == ':')
+            }
+        }
+    }
 }
+
